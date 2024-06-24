eb9100133f8c717a1329bd18322e7b7d30f9c6b9
rohit james <rohitjames77@outlook.com>
2024-06-24 20:19:38 +0530

    Added style.css and index.js files

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './style.css';
+const body = document.querySelector('body');
+const testDiv = document.createElement('h1');
+testDiv.id ='test';
+testDiv.textContent ='testing';
+body.appendChild(testDiv)
