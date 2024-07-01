76aa40cd0710e8988d14ed6049fa267d7487fbf3
rohit james <rohitjames77@outlook.com>
2024-07-01 20:47:38 +0530

    Modified files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 import "./style.css";
-import image from '../src/assets/todo-backdrop.jpg'
-import{sideBar,title,btnImage,card} from './Dom.js'
-import { fromUnixTime } from "date-fns";
-const body = document.querySelector("body");
-const testDiv = document.createElement("div");
-testDiv.style.backgroundImage= image;
-testDiv.id = "test";
-body.appendChild(testDiv);
+import { defaultDivParent } from "./defaultCard";
+import {body,parentDiv,childDiv} from './defaultCard.js'
+import { sideBar } from "./sideBar.js";
 
+
+
+
+
+
+
+
+// Sidebar Div .......................................
+// const sideBar = document.createElement('div');
+// sideBar.id= 'side-bar';
+// body.appendChild(sideBar);
+// // Sidebar Title and Logo ...............................
+// const title = document.createElement('h1');
+// title.id = 'side-bar-title';
+// title.textContent='To-Do';
+// sideBar.appendChild(title);
+// // new To Do create Button ................................
+// const btnImage = document.createElement('img');
+// btnImage.id ='btn-image';
+// btnImage.src=image;
+// btnImage.alt = 'btn-image'
+// body.appendChild(btnImage);
+// const card = document.createElement('div');
+// card.id ='card';
+// body.appendChild(card);
